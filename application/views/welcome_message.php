4f9676399f5526012f2cf0200e7019ffbc1911c5
rhielID <42262309+rhielID@users.noreply.github.com>
2018-08-13 11:01:13 +0700

    Coba rubah
    Coba

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Koperasi Bestari</title>
+	<title>.:Aplikasi Koperasi Bestari:.</title>
 
 	<style type="text/css">
 
